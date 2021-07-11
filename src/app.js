8105017fabee15625a9587d49b75ea642a507c16
Luís Henrique <61325756+henrique15775@users.noreply.github.com>
2021-07-11 13:33:46 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -5,6 +5,21 @@ require('dotenv').config()
 //const fetch = require('node-fetch');
 const moeda = require('./api/fetchData.js');
 const {imagens_win, imagens_lose} = require('./api/imagens.js');
+const AVLTree = require('avl');
+const tree_win = new AVLTree();
+const tree_lose = new AVLTree();
+
+counter = 0
+for(image of imagens_win){
+  tree_win.insert(counter,image);
+  counter+=1;
+}
+counter = 0;
+for(image of imagens_lose){
+  tree_lose.insert(counter,image);
+  counter+=1;
+}
+
 
 console.log(imagens_win);
 
@@ -17,58 +32,30 @@ const client = new Twitter({
   timeout_ms: 60*1000
 });
 
-/*var myHeaders = new Headers();
-
-var myInitRequest = { method: 'GET',
-               headers: myHeaders};
-*/
-/*
-const moeda = (url)=> {
-  const fat =  fetch(`${url}`).then(result => result);
-  return fat;
-}*/
-
 
 
 const doge_url = 'https://economia.awesomeapi.com.br/last/DOGE-USD';
 const bitcoin_url = 'https://economia.awesomeapi.com.br/last/BTC-USD';
-//console.log("Ola seja bem vindo ao bot\n");
-/*
-const imagens_loser = [
-  {
-    file: 'imagens_loser/imagem2-winner.jpg',
-    texto:''
-  }
 
-]
+const random_image = ( arvore ) => {
+  const key = Math.floor(Math.random() * arvore._size);
+  item = arvore.find(key);
+  return item.data;
 
-const imagens_winner = [
-  {
-  file: 'imagens_winner/imagem1-winner.jpg',
-  texto:''
-  }
-
-]
-*/
-
-
-//var imagem=  imagens[1];
-const random_image = ( images ) => {
-  return images[Math.floor( Math.random() * images.length )];
 }
 
 const posta_tweet = (bitcoin,dogecoin,imagem_escolhida) => {
 
-  var msg_loser = `Elon Musk perdeu 😭\n Valor do DogeCoin: $ ${dogecoin.bid}(${dogecoin.pctChange}%)\n Valor do BitCoin: $${bitcoin.bid}(${bitcoin.pctChange}%)`
-  var msg_winner = `Elon Musk ganhou 😎😎 \n Valor do DogeCoin: $ ${dogecoin.bid}(${dogecoin.pctChange}%)\n Valor do BitCoin: $${bitcoin.bid}(${bitcoin.pctChange}%)`
+  var msg_loser = `Elon Musk perdeu 😭\n Valor do DogeCoin: $${dogecoin.bid} (${dogecoin.pctChange}%)\n Valor do BitCoin: $${bitcoin.bid}(${bitcoin.pctChange}%)`
+  var msg_winner = `Elon Musk ganhou 😎😎 \n Valor do DogeCoin: $${dogecoin.bid} (${dogecoin.pctChange}%)\n Valor do BitCoin: $${bitcoin.bid}(${bitcoin.pctChange}%)`
 
   var resultado_luta = parseFloat(bitcoin.pctChange) > parseFloat(dogecoin.pctChange) ? msg_loser : msg_winner;
   imagem_escolhida.texto = resultado_luta;
 
   var imagem = path.join(__dirname,`/imagens/`+ imagem_escolhida.file);
   var b64content = fs.readFileSync(imagem, {encoding: 'base64'});
-  const postTweet = "...";
-  console.log('\n Executando...\n')
+
+  console.log('\n Executando...\n');
 
   client.post('media/upload', {media_data: b64content}, (err, data, response) => {
 
@@ -82,13 +69,15 @@ const posta_tweet = (bitcoin,dogecoin,imagem_escolhida) => {
         console.log("DEU RUIM!!  ERRO -> " + err);
         return -1;
       }
-      console.log('Imagem postada!!!')
+      console.log('Imagem postada!!!');
     })
 
     console.log('Tweet postado com sucesso!!\n\n');
 
 })
 }
+
+
 
 const executaBot = async(client) => {
 
@@ -97,72 +86,10 @@ const executaBot = async(client) => {
   get_dogecoin = dogecoin.DOGEUSD;
   get_bitcoin = bitcoin.BTCUSD;
   console.log(get_bitcoin);
-  let imagem_escolhida = parseFloat(get_dogecoin.pctChange) < parseFloat(get_bitcoin.pctChange) ? random_image(imagens_lose) : random_image(imagens_win);
-  posta_tweet(get_bitcoin,get_dogecoin,imagem_escolhida)
-  /*
-  var msg_loser = `Elon Musk perdeu 😭\n Valor do DogeCoin: $ ${dogecoin.DOGEUSD.bid}(${dogecoin.DOGEUSD.pctChange}%)\n Valor do BitCoin: $${bitcoin.BITUSD.bid}(${bitcoin.BITUSD.pctChange}%)`
-  var msg_winner = `Elon Musk ganhou 😎😎 \n Valor do DogeCoin: $ ${dogecoin.DOGEUSD.bid}(${dogecoin.DOGEUSD.pctChange}%)\n Valor do BitCoin: $${bitcoin.BITUSD.bid}(${bitcoin.BITUSD.pctChange}%)`
+  let imagem_escolhida = parseFloat(get_dogecoin.pctChange) < parseFloat(get_bitcoin.pctChange) ? random_image(tree_lose) : random_image(tree_win);
+  posta_tweet(get_bitcoin,get_dogecoin,imagem_escolhida);
 
-  var resultado_luta = bitcoin.BTCUSD.pctChange > dogecoin.DOGEUSD.pctChange ? msg_loser : msg_winner;
-  imagem_escolhida.texto = resultado_luta;
-
-  var imagem = path.join(__dirname,`src/imagens/imagens_winner/`+ imagem_escolhida.file);
-  var b64content = fs.readFileSync(imagem, {encoding: 'base64'});
-  const postTweet = "...";
-  console.log('\n Executando...\n')
-
-  client.post('media/upload', {media_data: b64content}, (err, data, response) => {
-
-    if(err){
-      console.log("DEU RUIM!!  ERRO -> " + err);
-      return -1;
-    }
-
-    client.post('statuses/update',{status:imagem_escolhida.texto,media_ids: new Array(data.media_id_string)},(err,data,response) => {
-      if(err){
-        console.log("DEU RUIM!!  ERRO -> " + err);
-        return -1;
-      }
-      console.log('Imagem postada!!!')
-    })
-
-    console.log('Tweet postado com sucesso!!\n\n');
-
-})*/
 
 }
 
 module.exports = executaBot;
-
-
-/*
-const a  = async () => {
-const dogecoin = await moeda(doge_url).then(resultado => resultado);
-const bitcoin  =  await moeda(bitcoin_url).then(resulta => resulta);
-const stringe = `${dogecoin.DOGEUSD.bid}\n\n ${bitcoin.BTCUSD.bid} `;
-console.log(stringe);
-*/
-//const var_dog = dogecoin.then(result => {result.json()});
-//const bitcoin  =  await moeda(b);
-  //return await dogecoin;
-//}
-
-//setInterval(a,5000);
-
-
-/*const oxi = async () => {
-  const dogecoin = await moeda(doge_url);
-  return await dogecoin;
-}
-const b = async () => {
-  con
-}*/
-
-
-//console.log(dogecoin);
-
-
-/*
-curl "https://api.twitter.com/2/tweets?ids=1261326399320715264,1278347468690915330"
- -H "Authorization: Bearer AAAAAAAAAAAAAAAAAAAAAFnz2wAAAAAAxTmQbp%2BIHDtAhTBbyNJon%2BA72K4%3DeIaigY0QBrv6Rp8KZQQLOTpo9ubw5Jt?WRE8avbi"
-*/
